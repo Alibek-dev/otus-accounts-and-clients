56c6659f3c1623456eb4cf268d202ea82087375e
Alibek <amyltykbayev@alseco.kz>
2022-02-19 00:49:52 +0600

    add generateAccounts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class Main {
@@ -23,5 +22,26 @@ public class Main {
         clients.add(new Client(9, "Суханов Назар Брониславович", 53));
         clients.add(new Client(10, "Зимин Марк Иванович", 22));
         return clients;
+    }
+
+    private List<Account> generateAccounts() {
+        List<Account> accounts = new ArrayList<Account>();
+        accounts.add(new Account(1, "Зимин Марк Иванович", 22000));
+        accounts.add(new Account(2, "Молчанов Глеб Викторович", 27000));
+        accounts.add(new Account(3, "Сафонов Гарри Эдуардович", 34000));
+        accounts.add(new Account(4, "Молчанов Глеб Викторович", 456000));
+        accounts.add(new Account(5, "Суханов Назар Брониславович", 237000));
+        accounts.add(new Account(6, "Сафонов Гарри Эдуардович", 55000));
+        accounts.add(new Account(7, "Петрив Донат Богданович", 4000));
+        accounts.add(new Account(8, "Сысоев Чарльз Иванович", 88000));
+        accounts.add(new Account(9, "Воронцов Цефас Викторович", 123000));
+        accounts.add(new Account(10, "Горбачёв Феликс Артёмович", 99000));
+        accounts.add(new Account(11, "Желиба Харитон Евгеньевич", 456000));
+        accounts.add(new Account(12, "Суханов Назар Брониславович", 556000));
+        accounts.add(new Account(13, "Петрив Донат Богданович", 4567000));
+        accounts.add(new Account(14, "Куликов Филипп Брониславович", 876000));
+        accounts.add(new Account(15, "Сафонов Гарри Эдуардович", 12000));
+        accounts.add(new Account(16, "Зимин Марк Иванович", 49000));
+        return accounts;
     }
 }
